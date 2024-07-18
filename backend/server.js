aba20edeb6e002aa33a52eb50a71af77118b41e7
LokeshN1 <lokeshnegi399@gmail.com>
2024-07-18 20:32:04 +0530

    Connecting with frontendURL

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // Enable CORS for requests from localhost:3000
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   credentials: true, // Allow sending cookies
 }));
 
