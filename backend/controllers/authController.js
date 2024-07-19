958e01574e0de32015599ad5a5e07f1f7356f65b
LokeshN1 <lokeshnegi399@gmail.com>
2024-07-19 11:16:38 +0530

    Updation on authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,17 +4,19 @@ const jwt = require('jsonwebtoken');
 const checkAuth = (req, res) => {
     // Retrieve the token from the HttpOnly cookie
     const token = req.cookies.token; // Adjust 'token' to your cookie name if different
-    // console.log("OKE"+token)
+
+    // Check if the token is present
     if (!token) {
-        return res.status(200).json({ isAuthenticated: false });
+        return res.status(401).json({ isAuthenticated: false, message: 'No token provided' });
     }
 
     // Verify the JWT token
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            console.log(err)
-            return res.status(200).json({ isAuthenticated: false });
+            console.error('Token verification error:', err); // Log error for debugging
+            return res.status(401).json({ isAuthenticated: false, message: 'Invalid token' });
         }
+        
         // If token is valid, user is authenticated
         return res.status(200).json({ isAuthenticated: true, admin: decoded.admin });
     });
